a6ef0d1803896f3a34829992ed7e61447a1b4073
james C. <cj.concepcion7@gmail.com>
2025-03-14 17:22:38 +0800

    leave module

---
+++ src/app/modules/leave/pages/leave-approvals/leave-approvals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-approvals',
+  imports: [],
+  templateUrl: './leave-approvals.component.html',
+  styleUrl: './leave-approvals.component.scss'
+})
+export class LeaveApprovalsComponent {
+
+}
---
+++ src/app/modules/leave/pages/leave-dashboard/leave-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-dashboard',
+  imports: [],
+  templateUrl: './leave-dashboard.component.html',
+  styleUrl: './leave-dashboard.component.scss'
+})
+export class LeaveDashboardComponent {
+
+}
