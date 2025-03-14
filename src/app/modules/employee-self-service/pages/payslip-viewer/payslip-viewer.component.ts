755baea6d53983d272cc38d7a604b484d73948ea
james C. <cj.concepcion7@gmail.com>
2025-03-14 17:21:47 +0800

    employee self module

---
+++ src/app/modules/employee-self-service/pages/payslip-viewer/payslip-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payslip-viewer',
+  imports: [],
+  templateUrl: './payslip-viewer.component.html',
+  styleUrl: './payslip-viewer.component.scss'
+})
+export class PayslipViewerComponent {
+
+}
---
+++ src/app/modules/employee-self-service/pages/timekeeping/timekeeping.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timekeeping',
+  imports: [],
+  templateUrl: './timekeeping.component.html',
+  styleUrl: './timekeeping.component.scss'
+})
+export class TimekeepingComponent {
+
+}
