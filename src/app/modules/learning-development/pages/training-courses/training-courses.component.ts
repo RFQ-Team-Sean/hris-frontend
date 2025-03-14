4fb54f80900ba8d4d470339036509c035a04ffa3
james C. <cj.concepcion7@gmail.com>
2025-03-14 17:24:17 +0800

    learn module

---
+++ src/app/modules/learning-development/pages/training-courses/training-courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-courses',
+  imports: [],
+  templateUrl: './training-courses.component.html',
+  styleUrl: './training-courses.component.scss'
+})
+export class TrainingCoursesComponent {
+
+}
