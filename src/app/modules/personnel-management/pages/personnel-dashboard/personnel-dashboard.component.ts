909dd40582914692adf6089cc7d23c6f05d79bbe
james C. <cj.concepcion7@gmail.com>
2025-03-14 17:21:25 +0800

    hr module

---
+++ src/app/modules/personnel-management/pages/personnel-dashboard/personnel-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnel-dashboard',
+  imports: [],
+  templateUrl: './personnel-dashboard.component.html',
+  styleUrl: './personnel-dashboard.component.scss'
+})
+export class PersonnelDashboardComponent {
+
+}
