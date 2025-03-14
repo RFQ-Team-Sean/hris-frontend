376835bfac8bbf90cdacc3d58dd1168698899b4b
james C. <cj.concepcion7@gmail.com>
2025-03-14 17:22:06 +0800

    time and attendance module

---
+++ src/app/modules/attendance/pages/attendance-dashboard/attendance-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-dashboard',
+  imports: [],
+  templateUrl: './attendance-dashboard.component.html',
+  styleUrl: './attendance-dashboard.component.scss'
+})
+export class AttendanceDashboardComponent {
+
+}
---
+++ src/app/modules/attendance/pages/attendance-reports/attendance-reports.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-reports',
+  imports: [],
+  templateUrl: './attendance-reports.component.html',
+  styleUrl: './attendance-reports.component.scss'
+})
+export class AttendanceReportsComponent {
+
+}
