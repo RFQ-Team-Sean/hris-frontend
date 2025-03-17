8e18d9f4c1f1f413fa98541aeeb24a11b02e2c88
james C. <cj.concepcion7@gmail.com>
2025-03-17 10:29:09 +0800

    sidebar routes

--- src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
+import { SidebarComponent } from '../../../../shared/components/sidebar/sidebar.component'; 
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
+  standalone: true, 
+  imports: [RouterModule, SidebarComponent], 
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
