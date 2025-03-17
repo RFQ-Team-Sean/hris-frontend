414db91911a456bdb3b7c9284b25f8a36a9710f4
james C. <cj.concepcion7@gmail.com>
2025-03-17 10:41:30 +0800

    dashboard

---
+++ src/app/modules/dashboard/pages/dashboard/payroll/payroll-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payroll-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './payroll-dashboard.component.html',
+  styleUrl: './payroll-dashboard.component.scss'
+})
+export class PayrollDashboardComponent {
+
+}
