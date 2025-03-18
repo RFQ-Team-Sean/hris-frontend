ad44c63d47fdf54f31bccb21e061dba24896e293
james C. <cj.concepcion7@gmail.com>
2025-03-19 01:01:36 +0800

    recruitement

--- src/app/modules/recruitment/pages/applicant-tracker/applicant-tracker.component.ts
+++ src/app/modules/recruitment/pages/applicant-tracker/applicant-tracker.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SidebarComponent } from '../../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-applicant-tracker',
   standalone: true,
+  imports: [RouterModule, SidebarComponent],
   templateUrl: './applicant-tracker.component.html',
   styleUrl: './applicant-tracker.component.scss'
 })
--- src/app/modules/recruitment/pages/job-listings/job-listings.component.ts
+++ src/app/modules/recruitment/pages/job-listings/job-listings.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SidebarComponent } from '../../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-job-listings',
   standalone: true,
+  imports: [RouterModule, SidebarComponent],
   templateUrl: './job-listings.component.html',
   styleUrl: './job-listings.component.scss'
 })
