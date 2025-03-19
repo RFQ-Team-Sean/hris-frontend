eb943fb3e18fa6472b6a7a335817fd84636fbea4
Angelo John S. Calleja <angelojohnse.calleja@bicol-u.edu.ph>
2025-03-19 18:16:59 +0800

    Enable standalone component for employee profile

--- src/app/modules/personnel-management/pages/employee-profile/employee-profile.component.ts
+++ src/app/modules/personnel-management/pages/employee-profile/employee-profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-employee-profile',
+  standalone: true,
   imports: [],
   templateUrl: './employee-profile.component.html',
   styleUrl: './employee-profile.component.scss'
