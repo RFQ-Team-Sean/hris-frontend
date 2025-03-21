7d997258b83b11263a19c34dc6095e506424e2a5
james C. <cj.concepcion7@gmail.com>
2025-03-21 10:17:52 +0800

    added api service

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = environment.apiUrl; // Use environment variable
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/data`);
+  }
+
+  postData(payload: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/data`, payload);
+  }
 }
