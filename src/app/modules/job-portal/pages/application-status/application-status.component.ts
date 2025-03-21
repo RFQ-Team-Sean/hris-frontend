dec6edc65363322e56beb3738f584f0b42b5ddc0
james C. <cj.concepcion7@gmail.com>
2025-03-21 10:14:52 +0800

    asd

--- src/app/modules/job-portal/pages/application-status/application-status.component.ts
+++ src/app/modules/job-portal/pages/application-status/application-status.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-application-status',
+  standalone: true,
   imports: [],
   templateUrl: './application-status.component.html',
   styleUrl: './application-status.component.scss'
--- src/app/modules/job-portal/pages/job-application/job-application.component.ts
+++ src/app/modules/job-portal/pages/job-application/job-application.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-job-application',
+  standalone: true,
   imports: [],
   templateUrl: './job-application.component.html',
   styleUrl: './job-application.component.scss'
