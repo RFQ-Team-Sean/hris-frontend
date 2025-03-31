b676a4bcd6d9363a2efc201705ae6ebfb74d4767
james C. <cj.concepcion7@gmail.com>
2025-03-31 14:30:09 +0800

    fix

--- src/app/modules/requests/requests-routing.module.ts
+++ src/app/modules/requests/requests-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RequestDashboardComponent } from './request-dashboard/request-dashboard.component';
 import { DtrAdjustmentsComponent } from './pages/dtr-adjustments/dtr-adjustments.component';
-import { LeaveRequestsComponent } from './pages/leave-requests/leave-requests.component';
 import { CertificationsMembershipComponent } from './pages/certifications-membership/certifications-membership.component';
 import { MonetizationRequestsComponent } from './pages/monetization-requests/monetization-requests.component';
 import { DocumentRequestsComponent } from './pages/document-requests/document-requests.component';
@@ -10,7 +9,6 @@ import { DocumentRequestsComponent } from './pages/document-requests/document-re
 const routes: Routes = [
   { path: 'request-dashboard', component: RequestDashboardComponent },
   { path: 'dtr-adjustments', component: DtrAdjustmentsComponent },
-  { path: 'leave-requests', component: LeaveRequestsComponent },
   { path: 'certifications-membership', component: CertificationsMembershipComponent },
   { path: 'monetization-requests', component: MonetizationRequestsComponent },
   { path: 'document-requests', component: DocumentRequestsComponent },
