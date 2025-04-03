c182d82f777f63978bfd10a60c8de37f700500b6
james C. <cj.concepcion7@gmail.com>
2025-04-03 11:21:51 +0800

    leave balance history

--- src/app/modules/leave/pages/leave-balance-history/leave-balance-history.component.ts
+++ src/app/modules/leave/pages/leave-balance-history/leave-balance-history.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../../../shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-leave-balance-history',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './leave-balance-history.component.html',
   styleUrl: './leave-balance-history.component.scss'
 })
