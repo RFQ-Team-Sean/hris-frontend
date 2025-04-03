a339b4f5c92f307f0df5bacc13961cbb36149605
james C. <cj.concepcion7@gmail.com>
2025-04-03 11:26:44 +0800

    Leave Application Status

--- src/app/modules/leave/pages/leave-application-status/leave-application-status.component.ts
+++ src/app/modules/leave/pages/leave-application-status/leave-application-status.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../../../shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-leave-application-status',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './leave-application-status.component.html',
   styleUrl: './leave-application-status.component.scss'
 })
