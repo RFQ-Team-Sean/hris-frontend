cf9835ef2564df8e7511360f0b6f4abeed068f8e
james C. <cj.concepcion7@gmail.com>
2025-04-03 13:33:58 +0800

    payroll dashboard

--- src/app/modules/dashboard/pages/dashboard/payroll/payroll-dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/payroll/payroll-dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; 
-import { SidebarComponent } from '../../../../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-payroll-dashboard',
   standalone: true,
-  imports: [RouterModule, SidebarComponent],
+  imports: [RouterModule],
   templateUrl: './payroll-dashboard.component.html',
   styleUrl: './payroll-dashboard.component.scss'
 })
